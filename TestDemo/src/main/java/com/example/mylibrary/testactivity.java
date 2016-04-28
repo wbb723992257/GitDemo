f723fd4db455de1a682e799d920d34587461425a
wbb <wbb@gmail.com>
2016-04-28 14:56:09 +0800

    Test demo to github

---
+++ TestDemo/src/main/java/com/example/mylibrary/testactivity.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testactivity);
+    }
+}
